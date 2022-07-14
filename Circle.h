fe9b2ec8d8d888a5f69eecfd87d8c94bceb512ad
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-07-14 07:23:37 +0000

    Initial commit

---
+++ Circle.h
@@ -0,0 +1,12 @@
+#pragma once
+class Circle {
+private:
+	int radius; 
+public:
+	Circle(); // Default Constructor
+	Circle(int r); // Overloaded Constructor
+	~Circle(); // Destructor
+	void display();
+	float calcArea();
+};
+
---
+++ Rectangle.h
@@ -0,0 +1,14 @@
+#pragma once
+class Rectangle
+{
+private:
+	int length; 
+	int width; 
+public:
+	Rectangle(); // Default Constructor
+	Rectangle(int l, int w); // Overloaded Constructor
+	~Rectangle(); // Destructor
+	int calcArea();
+	void display();
+};
+
